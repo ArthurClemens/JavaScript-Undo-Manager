540fadf6558d785880be3d8aedf2673c258f67c9
Arthur Clemens <arthur@visiblearea.com>
2011-05-10 23:48:16 +0200

    update date

--- js/undomanager_backbone.js
+++ js/undomanager_backbone.js
@@ -98,7 +98,7 @@ LICENSE
 
 The MIT License
 
-Copyright (c) 2010 Arthur Clemens, arthur@visiblearea.com
+Copyright (c) 2010-2011 Arthur Clemens, arthur@visiblearea.com
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
